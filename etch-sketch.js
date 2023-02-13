adc318e242d821e5d82588490a3dc9c0255eb5a0
Max King <ryan3311@hotmail.com>
2023-02-13 19:58:06 +0100

    git step 3 done

--- etch-sketch.js
+++ etch-sketch.js
@@ -1,9 +1,10 @@
 const sketch_board = document.querySelector(".container");
 let pixel_template = document.createElement("div");
 pixel_template.classList.add("pixel");
+const size = 16;
 
-for (let i=0; i < 16; i++) {
-    for (let i=0; i < 16; i++) {
+for (let i=0; i < size; i++) {
+    for (let i=0; i < size; i++) {
         pixel_template = pixel_template.cloneNode(true);
         sketch_board.appendChild(pixel_template);
     }
@@ -14,8 +15,4 @@ all_pixel.forEach( (pixel) => {
     pixel.addEventListener("mouseenter", () => {
         pixel.classList.add("hover");
     })
-    pixel.addEventListener("transitionend", (e) => {
-        if (e.propertyName !== "background-color") return;
-        pixel.classList.remove("hover");
-    });
 });
